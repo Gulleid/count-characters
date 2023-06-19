c70c9632fc818886c684e0970ba35cd2ec6b72e9
Gullied Mohamed <gulleidmohamed93@gmail.com>
2023-06-19 14:41:31 +0300

    count string

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Good Luck 💪🏾
+function char_count(str, array) 
+{
+ var array_Count = 0;
+ for (var guuleed = 0; guuleed < str.length; guuleed++) 
+ {
+    if (str.charAt(guuleed) == array) 
+      {
+      array_Count += 1;
+      }
+  }
+  return array_Count;
+}
+
+console.log(char_count('fikrcamp', 'a'));
+console.log(char_count('bootcamp','c'));
+console.log(char_count('fikrcamp coding bootcamp','o'));
